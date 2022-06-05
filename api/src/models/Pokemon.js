4bf72e820e6ef75e22a711eca0c5f8018676a03e
Julian Leyes <lg_julian@outlook.com>
2022-06-04 23:42:39 -0300

    Vavava

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,24 @@
-const { DataTypes } = require('sequelize');
+const { UUID, UUIDV4, INTEGER, STRING } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
+    nombre: {
+      type: STRING,
       allowNull: false,
     },
+    id: {
+      type: UUID,
+      defaultValue: UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    Vida: { type: INTEGER },
+    Ataque: { type: INTEGER },
+    Defensa: { type: INTEGER },
+    Velocidad: { type: INTEGER },
+    Altura: { type: INTEGER },
+    Peso: { type: INTEGER }
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const fetch = require('node-fetch');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -8,5 +9,32 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+let pokefiltro = (pokeinfo, pokearray) => {
+    let {name, height, weight, sprites, stats, types} = pokeinfo
+    pokearray.push({
+        nombre: name,
+        vida: stats[0].base_stat,
+        ataque: stats[1].base_stat,
+        defensa: stats[2].base_stat,
+        velocidad: stats[5].base_stat,
+        altura: height,
+        peso: weight,
+        imagen: sprites.other["official-artwork"].front_default,
+        tipo: types.map((poketype) => poketype.type.name)
+    })
+}
+
+router.get('/pokemons', async (req, res) => {
+    let pokemones = [];
+    for(let i = 1; i <= 40; i++) {
+        let pokedatos = await fetch (`https://pokeapi.co/api/v2/pokemon/${i}`)
+        .then(pokedata => pokedata.json())
+        await pokefiltro(pokedatos, pokemones)
+    }
+    res.status(200).send(pokemones)
+})
+
 
 module.exports = router;
+
+
